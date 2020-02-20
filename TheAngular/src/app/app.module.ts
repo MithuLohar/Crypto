68ae35eabe898fc66c9ed9f1b44740f68e1490e0
mithu <mithulohar@gmail.com>
2020-02-20 10:05:10 +0530

    CryptoApp

--- TheAngular/src/app/app.module.ts
+++ TheAngular/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { AuthGuard } from "./auth.guard";
+import { MatDialogModule } from "@angular/material";
+import { MatSliderModule } from "@angular/material/slider";
 import { HomeService } from "./home.service";
 import { AuthService } from "./auth.service";
 import { BrowserModule } from "@angular/platform-browser";
@@ -12,26 +14,40 @@ import { LoginComponent } from "./login/login.component";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { TokenInterceptorService } from "./token-interceptor.service";
 import { CountryService } from "./country.service";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { PopupComponent } from "./popup/popup.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    PopupComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatDialogModule
+  ],
   providers: [
     HomeService,
     AuthService,
     AuthGuard,
+
     CountryService,
+
     {
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptorService,
       multi: true
     }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [PopupComponent]
 })
 export class AppModule {}
